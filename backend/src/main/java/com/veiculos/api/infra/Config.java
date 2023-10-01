86128911fc9d10e57500ebe5801075ecbeb467cc
Péricles Tavares <periclesatavares@gmail.com>
2023-10-01 07:03:45 -0300

    Create User Service

---
+++ backend/src/main/java/com/veiculos/api/infra/Config.java
@@ -0,0 +1,28 @@
+package com.veiculos.api.infra;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * Class responsible configuration of spring beans for the app.
+ * @author Péricles Tavares
+ */
+@Configuration
+@PropertySource("classpath:ValidationMessages.properties")
+public class Config {
+
+    /**
+     * Create and config the ModelMapper for the DTO entity conversion.
+     * @return ModelMapper bean
+     */
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
+    }
+}
